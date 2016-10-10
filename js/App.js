9f110f96819632ab69e31293047f581ebf30307d
Trishin Dmitrii <smartkrio@yandex.ru>
2016-10-11 02:16:03 +0300

    Новости
    Расширил компоненты

--- js/App.js
+++ js/App.js
@@ -1,9 +1,57 @@
+var my_news = [
+  {
+    author: 'Саша Печкин',
+    text: 'В четверг, четвертого числа...'
+  },
+  {
+    author: 'Просто Вася',
+    text: 'Считаю что $ должен стоить 35 рублей!'
+  },
+  {
+    author: 'Гость',
+    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+  }
+];
+
+var News = React.createClass({
+  render: function(){
+    var data = this.props.data;
+    var newsTemplate;
+
+    if (data.length > 0) {
+      newsTemplate = data.map(function(item, index){
+        return(
+          <div key={index}>
+            <p className='news__author'>{item.author}:</p>
+            <p className='news__text'>{item.text}</p>
+          </div>
+        )
+      });
+    }else {
+      newsTemplate = <p>К сожалению - новостей нет!</p>
+    }
+
+    return (
+      <div className='news'>
+        {newsTemplate}
+        <strong className={data.length > 0 ? '' : 'none'}>Всего Новостей: {data.length}</strong>
+      </div>
+    );
+  }
+});
+
 var App = React.createClass({
   render: function(){
     return (
       <div className='app'>
-        Всем привет, я компонент App!
+        Всем привет, я компонент App! Я умею отображать новости.
+        <News data={my_news}/> {/*Добавили свойство data*/}
       </div>
     )
   }
-})
+});
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
